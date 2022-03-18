1414286628d3c0dd97af8adba99bce22ebccc4c7
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2022-03-18 15:09:28 -0300

    Concluindo o navegador 👻👽

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,8 +6,8 @@ import LinkButton from '../components/layout/LinkButton'
 function Home() {
   return (
     <sectio className={styles.home_container}>
-      <h1>Bem-vindo so <span>Costs</span></h1>
-      <p>Comece a gerenciar seus projetos agora mesmo!</p>
+      <h1>Bem-vindo ao <span>Vento Solar</span></h1>
+      <p>Garantindo um futuro com energia limpa e renovavel!</p>
       <LinkButton to="/novoprojeto" text="Criar projeto" />
       <img src={savings} alt="costs" />
     </sectio>
